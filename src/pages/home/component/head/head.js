776c2b225e8ef719291673f93c03351c9aa26b4c
twoelephant <1136742281@qq.com>
2022-08-18 11:18:05 +0800

    818

--- src/pages/home/component/head/head.js
+++ src/pages/home/component/head/head.js
@@ -5,10 +5,19 @@ import Avatar from '../../../../img/avatar.png';
 import { Button, Tooltip } from "antd";
 import { ExportOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+import { useState } from "react";
 
 function Head() {
     const navigate = useNavigate()
-    function handClick(){
+    const [username, setUsername] = useState("")
+
+    useEffect(() => {
+        let user = window.localStorage.getItem("username")
+        setUsername(user)
+    }, [])
+    function handClick() {
+        window.localStorage.clear()
         navigate('/')
     }
     return (
@@ -18,16 +27,16 @@ function Head() {
             </div>
             <div style={{ marginRight: '15px' }}>
                 <img src={Avatar} />
-                <span style={{margin:'0 15px'}}>
-                    欢迎您，admin
+                <span style={{ margin: '0 15px' }}>
+                    欢迎您，{username}
                 </span>
                 <Tooltip title="退出登录">
-                    <Button 
-                    onClick={()=>{
-                        handClick()
-                    }}
-                    style={{backgroundColor:'#5373e0',border:'none'}}
-                    type="primary" icon={<ExportOutlined />} />
+                    <Button
+                        onClick={() => {
+                            handClick()
+                        }}
+                        style={{ backgroundColor: '#5373e0', border: 'none' }}
+                        type="primary" icon={<ExportOutlined />} />
                 </Tooltip>
             </div>
         </div>
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -3,10 +3,11 @@ import './login.less';
 import 'antd/dist/antd.css';
 import Logo from '../../img/logo.png';
 import common from "./common/common";
-import { Form, Input, Button, } from 'antd';
+import { Form, Input, Button, message, } from 'antd';
 import { MobileOutlined, LockOutlined, SafetyCertificateOutlined } from '@ant-design/icons';   //logo
 import { useNavigate } from "react-router";
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
 
 function Login() {
@@ -15,17 +16,66 @@ function Login() {
     const [clientToken, setClientToken] = useState('')
     const [codeImage, setCodeImage] = useState('')
 
+    const [imgcode, setImgcode] = useState('')
+    const [imgurl, setImgurl] = useState('')
+    const [load, setLoad] = useState(false)
+
+
     const onFinish = (values) => {        //固定写法
         console.log('Received values of form: ', values);
+        axios({
+            method: 'get',
+            url: `http://106.15.65.54:8100/api/user/login`,
+            params: {
+                mobile: values.username,
+                captchaKey: imgcode,
+                captchaCode: values.code,
+                password: values.password,
+            }
+        }).then((res) => {
+            console.log(res);
+            if (res.data.message === "") {
+                setLoad(true)
+                window.localStorage.setItem("token", res.data.data)
+                window.localStorage.setItem("username", values.username)
+                navigate('/home')
+            } else {
+                message.warning(res.data.message)
+            }
+        }).catch((error) => {
+            console.log(error);
+        })
     };
+
+    var mytime
 
     // const handClick =() => {
     //     navigate('/home')
     // }
 
     useEffect(() => {
-        
+        clearTimeout(mytime)
+        mytime = setTimeout(() => {
+            time1()
+        }, 0);
     }, [])
+
+    const time1 = () => {
+        axios({
+            method: 'get',
+            url: `http://106.15.65.54:8100/api/user/captcha/create`,
+        }).then((res) => {
+            console.log(res);
+            setImgurl(res.data.data.url)
+            setImgcode(res.data.data.key)
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+
+    const handImg = () => {
+        time1()
+    }
 
     return (
         <div className="login-c">
@@ -72,9 +122,14 @@ function Login() {
                         <Input
                             // bordered={false}
                             prefix={<SafetyCertificateOutlined className="inputicon" />}
-                            placeholder="请输入验证码" />
+                            placeholder="请输入验证码"
+                            addonAfter={<img src={imgurl} alt=""
+                                onClick={handImg} />} />
+
                     </Form.Item>
-                    <Button type="primary" htmlType="submit">
+                    <Button type="primary"
+                        htmlType="submit"
+                        loading={load}>
                         登录
                     </Button>
                 </Form>
